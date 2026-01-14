4fcb9344d1b6f496bfe8f704cce7ffd7e45c4252
Prakhar Vohra <webfirex@gmail.com>
2026-01-14 12:05:49 +0530

    x

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import '@mantine/core/styles.css';
 
 import { createTheme, MantineProvider } from '@mantine/core';
 // import PublisherSignup from './signup/page';
-import HomePage from './homejhbjbhb/page';
+import HomePage from './home/page';
 
 const theme = createTheme({
   /** Put your mantine theme override here */
